7504c95eee739cd051f504fa177b2339fbd74379
go_bears <melissa@hackbrightacademy.com>
2016-01-24 03:55:54 +0000

    completed html forms, external css styling, flask logic&controller, jinga templating

---
+++ application.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request
+#import os import needed for deploying on cloud9
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index_page():
+    """Serves an index page."""
+    
+    return render_template("index.html")
+
+    # Alternately, we could make this a Jinja template in `templates/`
+    # and return that result of rendering this, like:
+    #
+    # return render_template("index.html")
+    
+@app.route("/application-form")
+def serve_application_form():
+    """Serves template application-form.html at the route /application-form """
+    
+    return render_template("application-form.html")
+    
+@app.route("/application", methods="POST")
+def confirm_form_submission():
+    """Serves the the template application-response.html at the route /application
+    
+    receives data from application-form.html and posts message to application-response.html
+    """
+    
+    first_name = request.form.get("firstname")
+    last_name = request.form.get("lastname")
+    salary = request.form.get("salary")
+    job = request.form.get("job")
+    print job
+    
+    submission_msg = """
+    Thank you, %s %s , for applying to be a %s. Your
+    minimum salary requirement is %s dollars.
+    """ % (first_name, last_name, job, salary)
+    
+    return render_template("application-response.html", msg = submission_msg)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    #for settings for deploying flask app on cloud9
+    #app.run(debug=True, host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
